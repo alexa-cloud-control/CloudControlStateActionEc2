6aded264dbe1fb848f4bd287034cc3d94fca1faf
Pawel Piwosz <pawel@pawelpiwosz.net>
2019-07-21 21:15:09 +0000

    initial commit

---
+++ py/CloudControlStateActionEc2.py
@@ -0,0 +1,46 @@
+import logging
+import boto3
+
+def CloudControlStateActionEc2(event, context):
+    """ Lambda function - start/stop/reboot ec2 """
+
+    # validate instance name
+    ec2 = boto3.resource('ec2')
+    ec2_client = boto3.client('ec2')
+    response = ec2_client.describe_instances(
+        Filters=[
+            {
+                'Name': 'tag:Name',
+                'Values': [event["body"]["InstanceName"]]
+            }
+        ]
+    )
+    instance_list = []
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            instance_list.append(instance['InstanceId'])
+    if not instance_list:
+        msg = "I cannot find the instance with name {}.".format(event["body"]["InstanceName"])
+        return 1, {"msg": msg}
+    # I should fix it somehow...
+    if event["body"]["InstanceState"] == 'start':
+        ec2_action = ec2.instances.filter(InstanceIds=instance_list).start()
+    elif event["body"]["InstanceState"] == 'reboot' or event["body"]["InstanceState"] == 'restart':
+        ec2_action = ec2.instances.filter(InstanceIds=instance_list).reboot()
+    elif event["body"]["InstanceState"] == 'stop':
+        ec2_action = ec2.instances.filter(InstanceIds=instance_list).stop()
+    elif event["body"]["InstanceState"] == 'hibernate':
+        msg = "Hibernate is not working now. I am sorry."
+        return 0, {"msg": msg}
+    else:
+        msg = "I do not know what to do, you use weird action to perform."
+        return 0, {"msg": msg}
+    msg = (
+        "{} against instance {} performed. "
+        "Check the state of the instance shortly, in order to control, "
+        "if {} action is successful.".format(
+            event["body"]["InstanceState"], event["body"]["InstanceName"], event["body"]["InstanceState"]
+        )
+    )
+    
+    return 0, {"msg": msg}
